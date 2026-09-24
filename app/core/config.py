a4ad488480c6154da5e062ddf397372b2441a6b8
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2026-09-24 22:36:38 +0700

    feat: add pydantic_settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,11 @@
+from pydantic_settings import BaseSettings
 class Settings:
+    PROJECT_NAME = "Project Chatme Cevin"
     API_V1_STR: str = "/api/v1"
+
+    AI_APIKEY : str
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
