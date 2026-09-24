0ec23ce26786fc6e318b68ee9b8cb963abb3512d
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2026-09-24 22:45:36 +0700

    fix: fix error env path in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,10 @@
-from pydantic_settings import BaseSettings
-class Settings:
-    PROJECT_NAME = "Project Chatme Cevin"
+from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Project Chatme Cevin"
     API_V1_STR: str = "/api/v1"
 
     AI_APIKEY : str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file = ".env", extra="ignore")
 
 settings = Settings()
